68fa08d38c911d10093eb9c893862b66d32bb52e
Ishita <ishitarawal16@gmail.com>
2022-01-04 13:23:53 +0530

    First Commit

--- Tripsters/settings.py
+++ Tripsters/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.travellers',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_rename_app',
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +80,7 @@ WSGI_APPLICATION = 'Tripsters.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':    BASE_DIR / 'db.sqlite3',
     }
 }
 
